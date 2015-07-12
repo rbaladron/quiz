4d46913b44dc660c40dbef16fa1000cda4175e8e
rbaladron <rbaladron@gmail.com>
2015-07-12 19:20:47 +0200

    Agregado Autoload

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,18 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
+// Página de entrada (home page)
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
+// Autoload de comando con :quizId
+router.param('quizId', quizController.load);  //autoload :quizId
+
 // Definición de rutas de /quizes
 rotuer.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
-router.get('/quizes/:quizID(\\d+)/answer', quizController.answer);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 /* GET página author */
 router.get('/author', function(req, res) {
