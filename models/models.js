6cc00d41947ce8933c26b3728ea94d694532907c
rbaladron <rbaladron@gmail.com>
2015-07-19 14:01:50 +0200

    Comentarios, autenticacion, sesión, moderación y HTTPS

--- models/models.js
+++ models/models.js
@@ -25,9 +25,20 @@ var sequelize = new Sequelize(DB_name, user, pwd, {
   omitNull: true // solo Postgres
 });
 
-// Importar la definición de la tabla Quiz en quiz.js
-var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+
+// Importar la definición de la tabla Quiz
+var quiz_path = path.join(__dirname, 'quiz');
+var Quiz = sequelize.import(quiz_path);
+
+// Importar definición de la tabla Comment
+var comment_path = path.join(__dirname, 'comment');
+var Comment = sequelize.import(comment_path);
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
 exports.Quiz = Quiz; //exportar definición de tabla Quiz
+exports.Comment = Comment; // exportar definición tabla Comment
 
 // sequelize.sync() inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
--- routes/index.js
+++ routes/index.js
@@ -12,17 +12,31 @@ router.get('/', function(req, res, next) {
 });
 
 // Autoload de comandos con :quizId
-router.param('quizId', quizController.load); //autoload :quizId
+router.param('quizId', quizController.load); // autoload :quizId
+router.paran('commentId', commentController.load);  // autoload :commentID
+
+// Definición de rutas de sesión
+router.get('/login', sessionController.new);        // Formulario login
+router.post('/login', sessionController.create);    // Crear sesión
+router.get('/logout', sessionController.destroy);   // Destruir sesión
 
 // Definición de rutas de /quizes
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
-router.get('/quizes/new', quizController.new);
-router.post('/quizes/create', quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
-router.put('/quizes/:quizId(\\d+)', quizController.update);
-router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
+router.get('/quizes/new', sessionController.loginRequired, quizController.new);
+router.post('/quizes/create', sessionController.loginRequired, quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit', sessionController.loginRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.destroy);
+
+// Definición de rutas de /comments
+router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
+
+
 // Créditos
 router.get('/quizes/creditos/author', quizController.author);
+
 module.exports = router;
