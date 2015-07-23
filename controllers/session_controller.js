75450548ba5ce654474f5056b14fdd4af6501e8c
rbaladron <rbaladron@gmail.com>
2015-07-23 19:51:39 +0200

    Entrega final con estadísticas v.2

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -52,7 +52,7 @@ exports.create = function(req, res) {
     }
 
     // Crear req.session.user y guardar campos id y username
-    // La sesión sde define por la existencia de: req.session.user
+    // La sesión se define por la existencia de: req.session.user
     req.session.user = {
       id: user.id,
       username: user.username
--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -18,7 +18,7 @@ exports.autenticar = function(login, password, callback) {
     if (password === users[login].password) {
       callback(null, users[login]);
     } else {
-      callback(new Error('Password incorrecto.'));
+      callback(new Error('Password erróneo.'));
     }
   } else {
     callback(new Error('No existe el usuario.'));
