44034b438d19bf43618a6b8279f71af1be49f445
rbaladron <rbaladron@gmail.com>
2015-07-29 00:09:23 +0200

    Entrega final con Estadísticas V.4

--- controllers/statistic_controller.js
+++ controllers/statistic_controller.js
@@ -3,15 +3,15 @@ var models = require('../models/models.js');
 var statistics = {
   quizes: 0,
   comments: 0,
-  commentsUnpublished: 0,
-  commentedQuizes: 0,
-  commentsUncomented: 0
+  average_comments: 0,
+  commented_quizes: 0,
+  no_commented: 0
 };
 
 var errors = [];
 
 
-exports.calculate = function(req, res, next) {
+/*exports.calculate = function(req, res, next) {
   models.Quiz.count()
    // preguntas
     .then(function(numQuizes) {
@@ -44,6 +44,31 @@ exports.calculate = function(req, res, next) {
     .finally(function() {
       next();
     });
+};*/
+
+exports.calculate = function(req, res, next) {
+  statistics.commented_quizes = 0;
+  statistics.no_commented = 0;
+  Promise.all([
+    models.Quiz.count(),
+    models.Comment.count(),
+    models.Quiz.findAll({
+      include: [{
+        model: models.Comment
+      }]
+    })
+  ]).then(function(results) { // `results` is an array of [quizes, comments, all]
+    statistics.quizes = results[0];
+    statistics.comments = results[1];
+    statistics.average_comments = (statistics.comments / statistics.quizes).toFixed(2);
+    for (var i in results[2]) {
+      if (results[2][i].comments.length) {
+        statistics.commented_quizes++;
+      } else {
+        statistics.no_commented++;
+      }
+    }
+  }).then(next, next);
 };
 
 
