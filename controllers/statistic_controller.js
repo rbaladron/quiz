0e22ec5c25fd535c32f42587be4a9cea48f808f5
rbaladron <rbaladron@gmail.com>
2015-07-29 18:51:41 +0200

    Entrega final con Estadísticas V.4

--- controllers/statistic_controller.js
+++ controllers/statistic_controller.js
@@ -3,15 +3,14 @@ var models = require('../models/models.js');
 var statistics = {
   quizes: 0,
   comments: 0,
-  average_comments: 0,
-  commented_quizes: 0,
-  no_commented: 0
+  numUnpublished: 0,
+  numCommented: 0
 };
 
 var errors = [];
 
 
-/*exports.calculate = function(req, res, next) {
+exports.calculate = function(req, res, next) {
   models.Quiz.count()
    // preguntas
     .then(function(numQuizes) {
@@ -31,45 +30,16 @@ var errors = [];
     // preguntas con comentarios
     .then(function(numCommented) {
       statistics.commentedQuizes = numCommented;
-      return (models.Quiz.count() - models.CountCommentedQuizes());
     })
-    // preguntas sin comentarios
-    .then(function(numUnCommented) {
-      statistics.commentsUncomented = numUnCommented;
-      //return (models.Quiz.count() - models.CountCommentedQuizes());
-    })
+
     .catch(function(err) {
       errors.push(err);
     })
     .finally(function() {
       next();
     });
-};*/
-
-exports.calculate = function(req, res, next) {
-  statistics.commented_quizes = 0;
-  statistics.no_commented = 0;
-  Promise.all([
-    models.Quiz.count(),
-    models.Comment.count(),
-    models.Quiz.findAll({
-      include: [{
-        model: models.Comment
-      }]
-    })
-  ]).then(function(results) { // `results` is an array of [quizes, comments, all]
-    statistics.quizes = results[0];
-    statistics.comments = results[1];
-    statistics.average_comments = (statistics.comments / statistics.quizes).toFixed(2);
-    for (var i in results[2]) {
-      if (results[2][i].comments.length) {
-        statistics.commented_quizes++;
-      } else {
-        statistics.no_commented++;
-      }
-    }
-  }).then(next, next);
 };
+
 
 
 exports.show = function(req, res) {
