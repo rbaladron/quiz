df030eef41c9d7657091d0f92591616fc6336873
rbaladron <rbaladron@gmail.com>
2015-08-03 21:46:53 +0200

    Entrega final con Estadísticas V.4

--- controllers/statistic_controller.js
+++ controllers/statistic_controller.js
@@ -1,39 +1,38 @@
 var models = require('../models/models.js');
 
 var statistics = {
-  quizes: 0,
-  comments: 0,
-  numUnpublished: 0,
-  numCommented: 0
-};
+      quizes: 0,
+      comments: 0,
+      commentsUnpublished: 0,
+      commentedQuizes:0,
+      commentsUncomented:0
+    };
 
 var errors = [];
 
 
-exports.calculate = function(req, res, next) {
-  models.Quiz.count()
-    .then(function(numQuizes) { // número de preguntas
-      statistics.quizes = numQuizes;
-      return models.Comment.count();
+exports.calculate = function (req, res, next) {
+   models.Quiz.count()
+   .then(function (numQuizes) { // número de preguntas
+   statistics.quizes = numQuizes;
+   return models.Comment.count();
+     })
+   .then(function (numComments) { // número de comentarios
+   statistics.comments = numComments;
+  return models.Comment.countUnpublished();
     })
-    .then(function(numComments) { // número de comentarios
-      statistics.comments = numComments;
-      return models.Comment.countUnpublished();
+  .then(function (numUnpublished) { // número de comentarios sin publicar
+   statistics.commentsUnpublished = numUnpublished;
+   return models.Comment.countCommentedQuizes();
     })
-    .then(function(numUnpublished) { // número de comentarios sin publicar
-      statistics.numUnpublished = numUnpublished;
-      return models.Comment.countUnpublished();
+   .then(function (numCommented) { // número de preguntas con comentario
+     statistics.commentedQuizes = numCommented;
+    return models.CountUnCommentedQuizes();
     })
-    .then(function(numCommented) { // número de preguntas con comentario
-      statistics.numCommented = numCommented;
-      return models.CountUnCommentedQuizes();
-    })
-    .catch(function(err) {
-      errors.push(err);
-    })
-    .finally(function() {
-      next();
-    });
+  .catch(function (err) { errors.push(err); })
+  .finally(function () {
+  next();
+  });
 };
 
 
